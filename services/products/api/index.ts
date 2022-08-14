35a6f6685287acc32b29b2220189d8a7ab1a266d
tabish <69778687+Tabish1-web@users.noreply.github.com>
2022-08-14 06:09:05 +0500

    test app and create docker-compose with mongo

--- services/products/api/index.ts
+++ services/products/api/index.ts
@@ -1,2 +1,14 @@
 import express, {Application} from "express";
 import dotenv from "dotenv"
+
+const app: Application = express();
+
+const port = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+    res.send('HELLO WORLD');
+})
+
+app.listen(port, () => {
+    console.log('server is running on ',port);
+});
